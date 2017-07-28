81480b3429a1f6391786b7181f5994ec162646ce
Ranjita Das <ranjita.renee.das@gmail.com>
2017-07-27 17:53:12 -0700

    Making sure things are connected

---
+++ server.js
@@ -0,0 +1,12 @@
+// import 'config' //takes it from node-module
+
+//we have to test if the server is working, and since we are using babel-node, we have to make sure that babel is installed globally. If stored locally we have to give the whole path everytime.
+
+//we can make ./node_modules/.bin/ as the global path as all the required files for this are stored inside this path
+
+import './config';
+
+console.log("testing 2");
+
+
+
